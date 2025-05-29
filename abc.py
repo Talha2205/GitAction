11c7c8303e81781828e7ed4085c5adb5edd794ef
Talha2205 <talhakhalil2790@gmail.com>
2025-05-29 15:10:29 +0500

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,17 +1,5 @@
 def subtract(a, b):
-    """
-    Subtracts the second number from the first number.
-
-    Parameters:
-    a (float or int): The number to subtract from.
-    b (float or int): The number to subtract.
-
-    Returns:
-    float or int: The result of a - b.
-    """
     return a - b
 
-# Example usage:
-result = subtract(10, 4)
-print("Result:", result)  # Output: Result: 6
+
 
